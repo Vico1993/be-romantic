2cae42b6e5b933fd588d1a02583cb3d1c76c6d02
Victor Piolin <victor.piolin@gmail.com>
2024-05-23 00:59:24 -0400

    feat(content): Display the content

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,34 +1,83 @@
 import React, { useEffect, useState } from "react";
 import { IEvent, relevantDate } from "../service/date-service";
 import { Col, Grid, Row } from "@zendeskgarden/react-grid";
+import { PALETTE } from "@zendeskgarden/react-theming";
+import { XXXL } from "@zendeskgarden/react-typography";
 import styled from "styled-components";
 
-const StyledRow = styled(Row)<{ index: number }>({
-    "&": {
-        fontSize: "40px",
-    },
-}) as typeof Row;
+import DateRow from "./../components/date-row";
+
+const StyledRowFooter = styled(Row)({
+    position: "fixed",
+    bottom: 0,
+    width: "100%",
+    paddingBottom: "20px",
+});
+
+const StyledGrid = styled(Grid)<{ minHeight: string }>((props) => ({
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: props.minHeight,
+    backgroundColor: PALETTE.yellow["200"],
+    fontFamily: "sans-serif",
+}));
+
+const StyledCoffeeImg = styled.img({
+    height: "41px",
+    width: "174px",
+    boxShadow: "0px 3px 2px 0px rgba(190, 190, 190, 0.5)",
+});
+
+const StyledRow = styled(Row)({
+    padding: "20px 0px",
+});
+
+const StyledTitleRow = styled(Row)({
+    position: "fixed",
+    top: "300px",
+    color: PALETTE.grey["800"],
+});
 
 export default function Index() {
     const [dates, setDates] = useState<IEvent[]>([]);
+    const { innerHeight } = window;
 
     useEffect(() => {
         setDates(relevantDate());
     }, []);
 
     return (
-        <Grid debug>
+        <StyledGrid minHeight={`${innerHeight - 16}px`}>
+            <StyledTitleRow className="title" alignItems="center">
+                <Col textAlign="center">
+                    <h1>
+                        <XXXL>When should I be romantic?</XXXL>
+                    </h1>
+                </Col>
+            </StyledTitleRow>
             {dates.map((date, i) => (
                 <StyledRow
                     key={`dates-row-${i}`}
                     className="dates-row"
-                    id={`dates-row-${i}`}
                     alignItems="center"
                 >
-                    <Col textAlign="center">{date.name}</Col>
+                    <Col textAlign="center">
+                        <DateRow date={date} index={i} />
+                    </Col>
                 </StyledRow>
             ))}
-            <Row>Hello World</Row>
-        </Grid>
+            <StyledRowFooter className="footer">
+                <Col textAlign="center">
+                    <a href="https://buymeacoffee.com/vico1993" target="_blank">
+                        <StyledCoffeeImg
+                            src="https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png"
+                            alt="Buy Me A Coffee"
+                        />
+                    </a>
+                </Col>
+            </StyledRowFooter>
+        </StyledGrid>
     );
 }
