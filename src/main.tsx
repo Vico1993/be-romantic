299e757d3fd49bb33daaaf0eb97d0e1daa7645b6
Victor Piolin <victor.piolin@gmail.com>
2024-05-27 22:34:08 -0400

    feat(analytics): Add google analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import * as React from "react";
+import ReactGA from "react-ga";
+import React, { useEffect } from "react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import ErrorPage from "./page/error";
 import IndexPage from "./page/index";
+
+const TRACKING_ID = "G-8QSMQ20PFL";
 
 const router = createBrowserRouter([
     {
@@ -13,8 +16,20 @@ const router = createBrowserRouter([
     },
 ]);
 
+function Main() {
+    ReactGA.initialize(TRACKING_ID);
+
+    useEffect(() => {
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }, []);
+
+    return (
+        <React.StrictMode>
+            <RouterProvider router={router} />
+        </React.StrictMode>
+    );
+}
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>
+    <Main />
 );
