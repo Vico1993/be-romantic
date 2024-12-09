502fd74ec8d71ea45495cbdcb04477199dfb7677
Victor Piolin <victor.piolin@gmail.com>
2024-12-09 11:52:32 -0500

    fix(main): Fix Main.tsx by including the theme provider from Garden

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { DEFAULT_THEME, ThemeProvider } from "@zendeskgarden/react-theming";
 
 import ErrorPage from "./page/error";
 import IndexPage from "./page/index";
@@ -15,6 +16,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <ThemeProvider theme={DEFAULT_THEME}>
+            <RouterProvider router={router} />
+        </ThemeProvider>
     </React.StrictMode>
 );
